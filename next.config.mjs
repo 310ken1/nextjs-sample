774c010832692a4258d29e28e4f0e0a42572006c
Kenichi Sato <310dev@gmail.com>
2025-02-02 13:59:03 +0900

    Static Site Generation (SSG)設定

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Static Exports
+  // https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+  output: "export",
+  // Output index.html
+  // https://nextjs.org/docs/app/api-reference/config/next-config-js/trailingSlash
+  trailingSlash: true,
+  // source maps
+  // https://nextjs.org/docs/app/api-reference/config/next-config-js/productionBrowserSourceMaps
+  productionBrowserSourceMaps: true,
+};
 
 export default nextConfig;
